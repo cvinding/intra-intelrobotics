9d62f23bff5e1cc8fdd039f2bb17f2147a293f62
tobias970 <tobias-97@outlook.dk>
2019-03-22 10:45:04 +0100

    Completed (Login)

---
+++ public_html/index.php
@@ -0,0 +1,43 @@
+
+<!DOCTYPE html>
+<html lang="dk">
+    <head>
+        <meta charset="UTF-8">
+        <title>Hjem</title>
+        <link rel="stylesheet" href="libs/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="libs/font-awesome/css/all.css">
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body style="background-color: #0c5460">
+
+    <div class="container">
+
+        <div class="loginbox media" style="border:1px solid black; border-radius: 5px; background-color: white">
+            <img class="d-flex align-self-center mr-3" src="/assets/loginimg.jpg" alt="Generic placeholder image" height="450px" width="350px" style="margin-left: 15px">
+            <div class="login media" style="border:1px solid black; border-radius: 5px; background-color: white">
+                <div class="media-body login">
+                    <h5 class="mt-0" style="text-align: center">Login</h5>
+                    <form class="login">
+                        <div class="form-group">
+                            <label for="formGroupExampleInput">Brugernavn</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Brugernavn">
+                        </div>
+                        <div class="form-group">
+                            <label for="formGroupExampleInput2">Kodeord</label>
+                            <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Kodeord">
+                        </div>
+                        <button type="button" class="btn btn-success container" style="margin-top: 20px">Success</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    </body>
+
+</html>
+
+<script src="libs/jquery/jquery-3.3.1.min.js"></script>
+<script src="libs/popper/popper.min.js"></script>
+<script src="libs/bootstrap-4.3.1-dist/js/bootstrap.min.js"></script>
