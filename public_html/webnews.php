3382e3243d4a961db0a037be21b62dc0c73c4bf2
czvr0 <chri656v@edu.sde.dk>
2019-03-27 14:22:17 +0100

    web delete

--- public_html/webnews.php
+++ public_html/webnews.php
@@ -84,7 +84,7 @@
                     Er du sikker på du vil slette nyheden?
                 </div>
                 <div class="modal-footer">
-                    <button type="button" id="delete-yes" class="btn btn-secondary" data-dismiss="modal">Ja</button>
+                    <button type="button" id="delete-yes" data-id="" class="btn btn-secondary" data-dismiss="modal">Ja</button>
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Nej</button>
                 </div>
             </div>
