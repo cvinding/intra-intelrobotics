8ce367e5c5d1e5cc41534665ce95f23176c632b7
czvr0 <chri656v@edu.sde.dk>
2019-03-27 11:39:12 +0100

    noget

--- public_html/js/addnews.js
+++ public_html/js/addnews.js
@@ -9,22 +9,19 @@ $(document).ready(function () {
         const uiBox = $(form.children(".ui-message"));
 
         if(!checkRequired(form, "Vigtigt","Skriv lige i felterne", uiBox)) {
-            console.log(form.serialize() + "&webDomain=" + webdomain());
+
+            request("/api/info/createNews", "POST", form.serialize() + "&webDomain=" + webdomain(), function (callback) {
+
+                uiBox.html(createDismissibleMessage("success", "Tilløk", "Du gjorde det"))
+                
+            })
+
+
         }
 
 
 
-        /*$.ajax({
-            type: "POST",
-            url: apiURL() + "/api/info/createNews",
-            data: form.serialize(),
-            success: function(result){
-
-            },
-            error: function (result) {
-                console.log(result);
-            }
-        });*/
+        /**/
 
     });
 
